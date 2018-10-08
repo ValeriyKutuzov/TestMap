9d2781fc3c6cb1cc81ccb4f559248ef6a24c018c
Valeriy Kutuzov <valeriy_kutuzov@epam.com>
2018-10-08 14:34:31 +0400

    Initial Commit

---
+++ TestMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestMap
+//
+//  Created by Valera Kutuzov on 08/10/2018.
+//  Copyright © 2018 EPAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
